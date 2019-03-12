e1587090d815505d7ffe3a198fcadbefdb085e9e
Michal Nedzka <michael.nedzka@gmail.com>
2019-03-12 20:21:18 +0100

    Add reducers, redux and react-redux packages

--- src/actions/authedUser.js
+++ src/actions/authedUser.js
@@ -1,4 +1,4 @@
-const SET_AUTH_USER = 'SET_AUTH_USER';
+export const SET_AUTH_USER = 'SET_AUTH_USER';
 
 export function setAuthedUser(id) {
   return { type: SET_AUTH_USER, id };
--- src/actions/polls.js
+++ src/actions/polls.js
@@ -1,4 +1,4 @@
-const RECEIVE_POLLS = 'RECEIVE_POLLS';
+export const RECEIVE_POLLS = 'RECEIVE_POLLS';
 
 export function receivePolls(polls) {
   return { type: RECEIVE_POLLS, polls };
