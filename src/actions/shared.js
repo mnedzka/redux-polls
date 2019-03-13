da3803d1b46d597bb4e4903cbe697134bf3cdd0d
Michal Nedzka <michael.nedzka@gmail.com>
2019-03-13 21:31:48 +0100

    Add loader with react-redux-loading package

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -2,15 +2,18 @@ import { getInitialData } from '../utils/api';
 import { receiveUsers } from './users';
 import { receivePolls } from './polls';
 import { setAuthedUser } from './authedUser';
+import { showLoading, hideLoading } from 'react-redux-loading';
 
 const AUTHED_ID = 'tylermcginnis';
 
 export function handleInitialData() {
   return dispatch => {
+    dispatch(showLoading());
     return getInitialData().then(({ users, polls }) => {
       dispatch(receiveUsers(users));
       dispatch(receivePolls(polls));
       dispatch(setAuthedUser(AUTHED_ID));
+      dispatch(hideLoading());
     });
   };
 }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,11 @@ import authedUser from './authedUser';
 import polls from './polls';
 import users from './users';
 import { combineReducers } from 'redux';
+import { loadingBarReducer } from 'react-redux-loading';
 
 export default combineReducers({
   authedUser,
   polls,
   users,
+  loadingBar: loadingBarReducer,
 });
