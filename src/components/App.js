2cbbfcef62a69e1fafad82dbd5322cc87e64fa98
Michal Nedzka <michael.nedzka@gmail.com>
2019-03-14 20:58:46 +0100

    Add Leaderboard component

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { handleInitialData } from '../actions/shared';
 import Dashboard from './Dashboard';
 import LoadingBar from 'react-redux-loading';
+import Leaderboard from './Leaderboard';
 
 class App extends Component {
   componentDidMount() {
@@ -15,7 +16,7 @@ class App extends Component {
     return (
       <div>
         <LoadingBar />
-        {loading === true ? null : <Dashboard />}
+        {loading === true ? null : <Leaderboard />}
       </div>
     );
   }
