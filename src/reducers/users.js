2c77ac08981c5b18922e671cc5096c8581fec492
Michal Nedzka <michael.nedzka@gmail.com>
2019-03-18 18:54:37 +0100

    Add aswers action and include it in users and polls reducers

--- src/reducers/users.js
+++ src/reducers/users.js
@@ -1,5 +1,6 @@
 import { RECEIVE_USERS } from '../actions/users';
 import { ADD_POLL } from '../actions/polls';
+import { ADD_ANSWER } from '../actions/answers';
 
 export default function users(state = {}, action) {
   switch (action.type) {
@@ -15,11 +16,20 @@ export default function users(state = {}, action) {
       return {
         ...state,
         [author]: {
-          ...state,
+          ...state[author],
           polls: state[author].polls.concat([id]),
         },
       };
+    case ADD_ANSWER:
+      const user = state[action.authedUser];
 
+      return {
+        ...state,
+        [action.authedUser]: {
+          ...user,
+          answers: user.answers.concat([action.id]),
+        },
+      };
     default:
       return state;
   }
